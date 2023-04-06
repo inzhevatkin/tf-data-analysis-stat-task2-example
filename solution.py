f0f581f15793b296d759e2211714b3b97e3340c1
inzhevatkin <51568160+inzhevatkin@users.noreply.github.com>
2023-04-06 23:31:56 +0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from math import log
+#from math import log
 from scipy.stats import expon
 
 
@@ -11,11 +11,14 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    n = len(x);
-    alfa = (1-p)/2;
-    za = -log(1-alfa)/n;
-    zb = -log(alfa)/n;
+    n = len(x)
+    alfa = 1 - p
+    za = expon.ppf(alpha / 2)
+    zb = expon.ppf(1 - alpha / 2)
+    #za = -log(1-alfa)/n;
+    #zb = -log(alfa)/n;
     loc = x.min()
-    t=71**2;
-    return (za - 0.5 + loc)/t, \
-           (zb - 0.5 + loc)/t
+    t=71**2
+    return (za - 0.5 + loc) / t, \
+           (zb - 0.5 + loc) / t
+
